cf0bfa1f59b31309c7f2938ad8a5f02099737538
Vadym Fesiuk <vadymfesiuk@gmail.com>
2023-11-13 11:09:34 +0100

    initial commit

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { ref } from 'vue'
+import { useTodoListStore } from '@/stores/todoList';
+
+const todo = ref('');
+
+const todoListStore = useTodoListStore();
+
+function addItemAndClear(item) {
+    if (item.length === 0) {
+        return;
+    }
+    todoListStore.addTodo(item); // Corrected line
+    todo.value = '';
+}
+</script>
+
+<template>
+    <div>
+        <form @submit.prevent="addItemAndClear(todo)">
+            <input v-model="todo" type="text"> <button>Add</button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+form {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 2vh;
+}
+input {
+    width: 50%;
+    padding: 1vh;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+}
+button {
+    padding: 1vh;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    margin-left: 1vw;
+}
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { useTodoListStore } from '../stores/todoList';
+import { storeToRefs } from 'pinia';
+
+const store = useTodoListStore();
+
+const { todoList } = storeToRefs(store);
+
+const { toggleCompleted, deleteTodo } = store;
+</script>
+
+<template>
+    <div v-for="todo in todoList" :key="todo.id" class="item">
+    <div class="content">
+        <span :class="{ completed: todo.comleted }">{{ todo.item }}</span>
+        <div>
+            <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+            <span @click="deleteTodo(todo.id)" class="x">&#10060;</span>
+        </div>
+    </div>
+    </div>
+</template>
+
+<style scoped> 
+span {
+    cursor: pointer;
+    margin: 0 10px;
+}
+
+.item {
+    display: flex;
+    justify-content: center;
+}
+
+.content {
+    display: flex;
+    justify-content: space-between;
+    width: 80vw;
+    padding: 5px;
+    font-size: 1.5em;
+
+}
+.completed {
+    text-decoration: line-through;
+} 
+</style>
+```
